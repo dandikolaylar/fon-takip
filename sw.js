49d14fcfd375cb2685ea69671444d5e178bd311e
dandikolaylar <157589845+dandikolaylar@users.noreply.github.com>
2026-07-10 11:28:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fon-takip-v7';
+const CACHE = 'fon-takip-v10';
 const FILES = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
